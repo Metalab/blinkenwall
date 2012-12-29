1a8b75d35100032848d9bbea6b581d3840d1830b
chrysn <chrysn@fsfe.org>
2012-12-29 21:49:55 +0100

    more flexible configuration

--- stdclients-py/blinkentrashclient.py
+++ stdclients-py/blinkentrashclient.py
@@ -1,14 +1,15 @@
-# for output on a `blinkentrash 40 10`
+# for output on a `blinkentrash 40 10` when called as ./blinkentrashclient.py 40 10
 
 import sys
 
+
 class Display(object):
     def send(self, data):
-        assert len(data) == 40 * 10
+        assert len(data) == self.width * self.height
         string = "".join(chr(r)+chr(g)+chr(b) for (r,g,b) in data)
-        assert len(string) == 40 * 10 * 3
+        assert len(string) == self.width * self.height * 3
         sys.stdout.write(string)
         sys.stdout.flush()
 
-    width = 40
-    height = 10
+    width = sys.argv[1]
+    height = sys.argv[2]
